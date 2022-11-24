015ccb0b9ddf735660f9764286489dacab0331e7
THEyarik <yaroslavpatcalon@gmail.com>
2022-11-24 14:55:57 +0200

    Add modal window

--- js/main.js
+++ js/main.js
@@ -15,3 +15,18 @@ for (let elm of elements) {
     observer.observe(elm);
 }
 
+//modal window
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
